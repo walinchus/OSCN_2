4d844b6fc13f6e5720630a1d8ffd95faceab826d
Lucia Walinchus <Lucia@professionalnewsservices.com>
2017-06-08 13:47:46 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
- import scraperwiki
+import scraperwiki
 import lxml.html
 import mechanize
 
