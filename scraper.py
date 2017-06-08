9b32ecacb80772ec291bfefac5a8481d669b1a64
Lucia Walinchus <Lucia@professionalnewsservices.com>
2017-06-08 17:47:37 -0500

    Create scraper.py

--- scraper.py
+++ scraper.py
@@ -4,6 +4,35 @@ import scraperwiki
 import lxml.html
 import mechanize
 
+def scrape_table(root):
+    #grab all table rows <tr> in table class="tblSearchResults"
+    rows = root.cssselect("table.caseCourtTable tr")
+    #create an ID number set at 0 - will add 1 every time we store a record (below)
+    idno = 0
+    #create a record to hold the data
+    record = {}
+    #for each row, loop through this
+    for row in rows:
+        #create a list of all cells <td> in that row
+        table_cells = row.cssselect("td")
+        if table_cells: 
+        #if there is a cell, record the contents in our dataset, the first cell [0] in 'recipient' and so on
+            record['Case Number'] = table_cells[0].text_content()
+            record['Date Filed'] = table_cells[1].text_content()
+            #this line adds 1 to the ID no. we set at 0 earlier
+            #idno=idno+1
+            #record['ID'] = idno 
+            record['Caption'] = table_cells[2].text_content()
+            record['Found Party'] = table_cells[3].text_content()
+            table_cellsurls = table_cells[0].cssselect("a")
+            #grab the href=" attribute of the first <a ... and store
+            record['URL'] = table_cellsurls[0].attrib.get('href')
+                # Print out the data we've gathered
+            print record, '------------'
+            # Save the record to the datastore - 'ID' is our unique key - 
+            scraperwiki.sqlite.save(["Case Number"], record)
+
+
 
 br = mechanize.Browser()
 br.set_handle_robots( False )
