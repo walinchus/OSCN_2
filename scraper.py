2865e4db73c9b0e3ee5a618c5660cd2da903eef1
Lucia Walinchus <Lucia@professionalnewsservices.com>
2017-08-08 12:01:20 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -35,7 +35,7 @@ def scrape_table(root):
 
 
 br = mechanize.Browser()
-br.set_all_readonly(False)    # allow everything to be written to
+#br.set_all_readonly(False)    # allow everything to be written to
 br.set_handle_robots(False)   # ignore robots
 br.set_handle_refresh(False)  # can sometimes hang without this
 br.addheaders = [('User-agent', 'Firefox')]   	      	# [('User-agent', 'Firefox')]
